1245e7137a1672531d4d170c2509cf6eed75c5cf
Alexander Rao <alexhrao@gmail.com>
2021-06-01 03:00:30 -0500

    Fix prettier and add array check

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,159 +6,161 @@ import yargs from 'yargs/yargs';
 import { hideBin } from 'yargs/helpers';
 
 import {
-  addDevice,
-  completeDevice,
-  createUser,
-  Device,
-  generateHandle,
-  getUser
+    addDevice,
+    completeDevice,
+    createUser,
+    Device,
+    generateHandle,
+    getUser
 } from './user';
 import {
-  DEInitResponse,
-  HashAlgorithm,
-  isDEInit,
-  NONCE_LEN,
-  SALT_LEN,
-  isDEComplete,
-  DeviceType,
-  isWSAuth,
-  isNewUser
+    DEInitResponse,
+    HashAlgorithm,
+    isDEInit,
+    NONCE_LEN,
+    SALT_LEN,
+    isDEComplete,
+    DeviceType,
+    isWSAuth,
+    isNewUser
 } from '../shared/types/authentication';
 import { fingerprint, generateNonce } from '../shared/auth';
 import {
-  AuthMessage,
-  ErrorMessage,
-  isClientMessage,
-  MessageType
+    AuthMessage,
+    ErrorMessage,
+    isClientMessage,
+    MessageType
 } from '../shared/types/Message';
 
 interface ServerOptions extends Arguments {
-  'server-port'?: number;
-  'socket-port'?: number;
+    'server-port'?: number;
+    'socket-port'?: number;
 }
 
 const argv = yargs(hideBin(process.argv)).options({
-  'server-port': {
-    type: 'number',
-    describe:
-      'The port to serve the HTTP express application from. Defaults to $SERVER_PORT or 3000.'
-  },
-  'socket-port': {
-    type: 'number',
-    describe:
-      'The port to serve the WebSocket server from. Defaults to $SOCKET_PORT or 8080.'
-  }
+    'server-port': {
+        type: 'number',
+        describe:
+            'The port to serve the HTTP express application from. Defaults to $SERVER_PORT or 3000.'
+    },
+    'socket-port': {
+        type: 'number',
+        describe:
+            'The port to serve the WebSocket server from. Defaults to $SOCKET_PORT or 8080.'
+    }
 }).argv as ServerOptions;
 
 interface DeviceSocket {
-  userID: string;
-  deviceID: string;
-  fingerprint: string;
-  socket: WebSocket;
+    userID: string;
+    deviceID: string;
+    fingerprint: string;
+    socket: WebSocket;
 }
 interface ConnectionLibrary {
-  [userID: string]: {
-    [deviceID: string]: DeviceSocket;
-  };
+    [userID: string]: {
+        [deviceID: string]: DeviceSocket;
+    };
 }
 // Index with userID, then with deviceID
 const conns: ConnectionLibrary = {};
 
 const httpPort =
-  argv['server-port'] ?? parseInt(process.env['SERVER_PORT'] ?? '3000');
+    argv['server-port'] ?? parseInt(process.env['SERVER_PORT'] ?? '3000');
 const wsPort =
-  argv['socket-port'] ?? parseInt(process.env['SOCKET_PORT'] ?? '8080');
+    argv['socket-port'] ?? parseInt(process.env['SOCKET_PORT'] ?? '8080');
 
 // WebSockets
 const wss = new Server({
-  port: wsPort
+    port: wsPort
 });
 
 wss.on('listening', () => {
-  console.log(`SocketServer listening on port ${wsPort}`);
+    console.log(`SocketServer listening on port ${wsPort}`);
 });
 
 wss.on('connection', (ws) => {
-  let authed = false;
-  let dev: DeviceSocket;
-  ws.on('message', async (data) => {
-    // client does first volley...
-    if (!authed) {
-      try {
-        const msg = JSON.parse(data.toString('utf-8'));
-        if (!isWSAuth(msg)) {
-          throw new Error('Invalid Payload');
-        }
-        // check it
-        const user = await getUser(msg.userID);
-        if (user.devices[msg.deviceID]?.fingerprint === msg.fingerprint) {
-          // check if our user is there
-          if (!(msg.userID in conns)) {
-            conns[msg.userID] = {};
-          }
-          if (msg.deviceID in conns[msg.userID]) {
-            // wut
-            throw new Error('Device already listening...');
-          }
-          conns[msg.userID][msg.deviceID] = {
-            deviceID: msg.deviceID,
-            fingerprint: msg.fingerprint,
-            userID: msg.userID,
-            socket: ws
-          };
-          const closer = () => {
-            delete conns[msg.userID][msg.deviceID];
-            if (Object.keys(conns[msg.userID]).length === 0) {
-              delete conns[msg.userID][msg.deviceID];
+    let authed = false;
+    let dev: DeviceSocket;
+    ws.on('message', async (data) => {
+        // client does first volley...
+        if (!authed) {
+            try {
+                const msg = JSON.parse(data.toString('utf-8'));
+                if (!isWSAuth(msg)) {
+                    throw new Error('Invalid Payload');
+                }
+                // check it
+                const user = await getUser(msg.userID);
+                if (
+                    user.devices[msg.deviceID]?.fingerprint === msg.fingerprint
+                ) {
+                    // check if our user is there
+                    if (!(msg.userID in conns)) {
+                        conns[msg.userID] = {};
+                    }
+                    if (msg.deviceID in conns[msg.userID]) {
+                        // wut
+                        throw new Error('Device already listening...');
+                    }
+                    conns[msg.userID][msg.deviceID] = {
+                        deviceID: msg.deviceID,
+                        fingerprint: msg.fingerprint,
+                        userID: msg.userID,
+                        socket: ws
+                    };
+                    const closer = () => {
+                        delete conns[msg.userID][msg.deviceID];
+                        if (Object.keys(conns[msg.userID]).length === 0) {
+                            delete conns[msg.userID][msg.deviceID];
+                        }
+                    };
+                    ws.on('error', closer);
+                    ws.on('close', closer);
+                    dev = conns[msg.userID][msg.deviceID];
+                    const success: AuthMessage = {
+                        type: MessageType.AACK
+                    };
+                    ws.send(JSON.stringify(success));
+                    authed = true;
+                } else {
+                    throw new Error('Invalid Fingerprint');
+                }
+            } catch (e) {
+                const err: ErrorMessage = {
+                    type: MessageType.ERR,
+                    errNo: NaN
+                };
+                ws.send(JSON.stringify(err));
+                ws.close();
             }
-          };
-          ws.on('error', closer);
-          ws.on('close', closer);
-          dev = conns[msg.userID][msg.deviceID];
-          const success: AuthMessage = {
-            type: MessageType.AACK
-          };
-          ws.send(JSON.stringify(success));
-          authed = true;
-        } else {
-          throw new Error('Invalid Fingerprint');
+            return;
         }
-      } catch (e) {
-        const err: ErrorMessage = {
-          type: MessageType.ERR,
-          errNo: NaN
-        };
-        ws.send(JSON.stringify(err));
-        ws.close();
-      }
-      return;
-    }
-    try {
-      const msg = JSON.parse(data.toString('utf8'));
-      if (!isClientMessage(msg)) {
-        throw new Error('Invalid Payload');
-      }
-      // read the destination, send accordingly
-      // so... eventually, we'll need to manage queues for users with devices that aren't online...
-      // but I don't wanna do that right now. So for now, we're just going to send to devices that are awake
-      const dst = typeof msg.dst === 'string' ? msg.dst : msg.dst.userID;
-      if (!(dst in conns)) {
-        return;
-      } else {
-        msg.timeServer = Date.now();
-        Object.values(conns[dst]).forEach((ds) => {
-          ds.socket.send(JSON.stringify(msg));
-        });
-      }
-    } catch (e) {
-      const err: ErrorMessage = {
-        type: MessageType.ERR,
-        errNo: NaN
-      };
-      ws.send(JSON.stringify(err));
-      return;
-    }
-  });
+        try {
+            const msg = JSON.parse(data.toString('utf8'));
+            if (!isClientMessage(msg)) {
+                throw new Error('Invalid Payload');
+            }
+            // read the destination, send accordingly
+            // so... eventually, we'll need to manage queues for users with devices that aren't online...
+            // but I don't wanna do that right now. So for now, we're just going to send to devices that are awake
+            const dst = typeof msg.dst === 'string' ? msg.dst : msg.dst.userID;
+            if (!(dst in conns)) {
+                return;
+            } else {
+                msg.timeServer = Date.now();
+                Object.values(conns[dst]).forEach((ds) => {
+                    ds.socket.send(JSON.stringify(msg));
+                });
+            }
+        } catch (e) {
+            const err: ErrorMessage = {
+                type: MessageType.ERR,
+                errNo: NaN
+            };
+            ws.send(JSON.stringify(err));
+            return;
+        }
+    });
 });
 
 // express!
@@ -166,76 +168,76 @@ const httpServer = express();
 httpServer.use(express.json());
 
 httpServer.get('/users', async (req, res) => {
-  if (req.query['username'] !== undefined) {
-    generateHandle().then((handle) => {
-      res.contentType('text/plain').send(handle).end();
-    });
-  } else {
-    res.sendStatus(404);
-  }
+    if (req.query['username'] !== undefined) {
+        generateHandle().then((handle) => {
+            res.contentType('text/plain').send(handle).end();
+        });
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 httpServer.post('/users', async (req, res) => {
-  if (!isNewUser(req.body)) {
-    res.sendStatus(400);
-    return;
-  }
-  await createUser(req.body);
-  res.sendStatus(204);
+    if (!isNewUser(req.body)) {
+        res.sendStatus(400);
+        return;
+    }
+    await createUser(req.body);
+    res.sendStatus(204);
 });
 
 httpServer.post('/enroll', async (req, res) => {
-  // verify req.body
-  if (isDEInit(req.body)) {
-    const payload = req.body;
-    // look up user... how?
-    // TODO: Look @ local mongoDB stuff
-    // for now... can't find user. so just send back a random nonce + random salt
-    const resp: DEInitResponse = {
-      nonce: (await generateNonce(NONCE_LEN)).toString('base64'),
-      hashAlgorithm: HashAlgorithm.SHA256,
-      salt: ''
-    };
-    try {
-      const user = await getUser(payload.userID);
-      // success! fill in the salt, attach our hash (for checking later...)
-      resp.salt = user.salt;
-      const print = fingerprint({
-        deviceID: payload.deviceID,
-        hashAlg: user.hashalg,
-        nonce: resp.nonce,
-        passHash: user.passhash
-      });
-      addDevice(payload.userID, payload.deviceID, print);
-    } catch (e) {
-      // couldn't find! fill in w/ random salt
-      resp.salt = (await generateNonce(SALT_LEN)).toString('base64');
-    }
-    res.json(resp);
-  } else if (isDEComplete(req.body)) {
-    const payload = req.body;
-    try {
-      const dev: Device = {
-        fingerprint: payload.hash,
-        verified: true,
-        id: payload.deviceID,
-        info: {
-          name: payload.info?.name ?? payload.deviceID,
-          os: payload.info?.os ?? 'Unknown',
-          type: payload.info?.type ?? DeviceType.Unknown
+    // verify req.body
+    if (isDEInit(req.body)) {
+        const payload = req.body;
+        // look up user... how?
+        // TODO: Look @ local mongoDB stuff
+        // for now... can't find user. so just send back a random nonce + random salt
+        const resp: DEInitResponse = {
+            nonce: (await generateNonce(NONCE_LEN)).toString('base64'),
+            hashAlgorithm: HashAlgorithm.SHA256,
+            salt: ''
+        };
+        try {
+            const user = await getUser(payload.userID);
+            // success! fill in the salt, attach our hash (for checking later...)
+            resp.salt = user.salt;
+            const print = fingerprint({
+                deviceID: payload.deviceID,
+                hashAlg: user.hashalg,
+                nonce: resp.nonce,
+                passHash: user.passhash
+            });
+            addDevice(payload.userID, payload.deviceID, print);
+        } catch (e) {
+            // couldn't find! fill in w/ random salt
+            resp.salt = (await generateNonce(SALT_LEN)).toString('base64');
         }
-      };
-      await completeDevice(payload.userID, dev);
-      res.sendStatus(204);
-    } catch (e) {
-      res.sendStatus(401);
+        res.json(resp);
+    } else if (isDEComplete(req.body)) {
+        const payload = req.body;
+        try {
+            const dev: Device = {
+                fingerprint: payload.hash,
+                verified: true,
+                id: payload.deviceID,
+                info: {
+                    name: payload.info?.name ?? payload.deviceID,
+                    os: payload.info?.os ?? 'Unknown',
+                    type: payload.info?.type ?? DeviceType.Unknown
+                }
+            };
+            await completeDevice(payload.userID, dev);
+            res.sendStatus(204);
+        } catch (e) {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(400);
+        return;
     }
-  } else {
-    res.sendStatus(400);
-    return;
-  }
 });
 
 httpServer.listen(httpPort, () => {
-  console.log(`HTTPServer listening on port ${httpPort}`);
+    console.log(`HTTPServer listening on port ${httpPort}`);
 });
--- src/server/user.ts
+++ src/server/user.ts
@@ -1,8 +1,8 @@
 import { hashPassword } from '../shared/auth';
 import {
-  DeviceType,
-  HashAlgorithm,
-  NewUserPayload
+    DeviceType,
+    HashAlgorithm,
+    NewUserPayload
 } from '../shared/types/authentication';
 import { promises as fs } from 'fs';
 import { randomInt } from 'crypto';
@@ -12,151 +12,151 @@ const NUM_LEN = 5;
 const HANDLE_TTL = 5 * 60 * 1000;
 
 const adjectives = fs
-  .readFile(join(__dirname, 'adjectives.txt'))
-  .then((buf) => buf.toString('utf-8'))
-  .then((str) => str.split(/\r\n|\n\r|\n|\r/g));
+    .readFile(join(__dirname, 'adjectives.txt'))
+    .then((buf) => buf.toString('utf-8'))
+    .then((str) => str.split(/\r\n|\n\r|\n|\r/g));
 
 const nouns = fs
-  .readFile(join(__dirname, 'nouns.txt'))
-  .then((buf) => buf.toString('utf-8'))
-  .then((str) => str.split(/\r\n|\n\r|\n|\r/g));
+    .readFile(join(__dirname, 'nouns.txt'))
+    .then((buf) => buf.toString('utf-8'))
+    .then((str) => str.split(/\r\n|\n\r|\n|\r/g));
 
 export interface Device {
-  id: string;
-  fingerprint: string;
-  verified: boolean;
-  info: {
-    name: string;
-    os: string;
-    type: DeviceType;
-  };
+    id: string;
+    fingerprint: string;
+    verified: boolean;
+    info: {
+        name: string;
+        os: string;
+        type: DeviceType;
+    };
 }
 // This is only at server, so... no need to make this shared
 export interface User {
-  handle: string;
-  passhash: string;
-  salt: string;
-  hashalg: HashAlgorithm;
-  createdate: number; // Unix timestamp in UTC?
-  devices: {
-    [deviceID: string]: Device;
-  };
+    handle: string;
+    passhash: string;
+    salt: string;
+    hashalg: HashAlgorithm;
+    createdate: number; // Unix timestamp in UTC?
+    devices: {
+        [deviceID: string]: Device;
+    };
 }
 
 function properNoun(str: string): string {
-  return str.charAt(0).toLocaleUpperCase() + str.slice(1).toLocaleLowerCase();
+    return str.charAt(0).toLocaleUpperCase() + str.slice(1).toLocaleLowerCase();
 }
 
 export async function generateHandle(): Promise<string> {
-  const adj = await adjectives;
-  const noun = await nouns;
+    const adj = await adjectives;
+    const noun = await nouns;
 
-  const accountNum = randomInt(0, 10 ** NUM_LEN);
-  const first = properNoun(adj[randomInt(0, adj.length)]);
-  const second = properNoun(noun[randomInt(0, noun.length)]);
+    const accountNum = randomInt(0, 10 ** NUM_LEN);
+    const first = properNoun(adj[randomInt(0, adj.length)]);
+    const second = properNoun(noun[randomInt(0, noun.length)]);
 
-  const handle = `${first}${second}#${accountNum}`;
+    const handle = `${first}${second}#${accountNum}`;
 
-  // store it in db...
+    // store it in db...
 
-  // Check it doesn't already exist... if it does, do this again?
-  /*
+    // Check it doesn't already exist... if it does, do this again?
+    /*
   if (false) {
     return generateHandle();
   }
   */
-  // in 5 minutes, kill it!
-  setTimeout(() => {
-    // remove it...
-    console.log(`@${handle} was removed`);
-  }, HANDLE_TTL);
-  return handle;
+    // in 5 minutes, kill it!
+    setTimeout(() => {
+        // remove it...
+        console.log(`@${handle} was removed`);
+    }, HANDLE_TTL);
+    return handle;
 }
 
 export async function createUser(user: NewUserPayload): Promise<void> {
-  // get a salt
-  const hash = await hashPassword(user.password, HashAlgorithm.SHA256);
-  const payload: User = {
-    handle: user.handle,
-    createdate: Date.now(),
-    devices: {},
-    hashalg: hash.alg,
-    passhash: hash.hash,
-    salt: hash.salt
-  };
-  // TODO: Save somewhere...?
+    // get a salt
+    const hash = await hashPassword(user.password, HashAlgorithm.SHA256);
+    const payload: User = {
+        handle: user.handle,
+        createdate: Date.now(),
+        devices: {},
+        hashalg: hash.alg,
+        passhash: hash.hash,
+        salt: hash.salt
+    };
+    // TODO: Save somewhere...?
 }
 
 async function updateUser(user: User): Promise<void> {
-  // TODO: Save to DB!
+    // TODO: Save to DB!
 }
 
 export async function getUser(handle: string): Promise<User> {
-  throw new Error('Not Implemented');
+    throw new Error('Not Implemented');
 }
 
 export async function revokeDevice(
-  handle: string,
-  deviceID: string,
-  print: string
+    handle: string,
+    deviceID: string,
+    print: string
 ): Promise<void> {
-  try {
-    const user = await getUser(handle);
-    if (
-      deviceID in user.devices &&
-      user.devices[deviceID].fingerprint === print
-    ) {
-      delete user.devices[deviceID];
-      await updateUser(user);
+    try {
+        const user = await getUser(handle);
+        if (
+            deviceID in user.devices &&
+            user.devices[deviceID].fingerprint === print
+        ) {
+            delete user.devices[deviceID];
+            await updateUser(user);
+        }
+    } catch (e) {
+        // swallow errors!
     }
-  } catch (e) {
-    // swallow errors!
-  }
 }
 
 export async function addDevice(
-  handle: string,
-  deviceID: string,
-  print: string
+    handle: string,
+    deviceID: string,
+    print: string
 ): Promise<void> {
-  const user = await getUser(handle);
-  if (deviceID in user.devices && user.devices[deviceID].verified) {
-    // bad!
-    throw new Error('Device already exists');
-  }
-  user.devices[deviceID] = {
-    verified: false,
-    fingerprint: print,
-    id: deviceID,
-    info: {
-      name: 'Unverified',
-      os: 'Unverified',
-      type: DeviceType.Unknown
+    const user = await getUser(handle);
+    if (deviceID in user.devices && user.devices[deviceID].verified) {
+        // bad!
+        throw new Error('Device already exists');
     }
-  };
-  await updateUser(user);
+    user.devices[deviceID] = {
+        verified: false,
+        fingerprint: print,
+        id: deviceID,
+        info: {
+            name: 'Unverified',
+            os: 'Unverified',
+            type: DeviceType.Unknown
+        }
+    };
+    await updateUser(user);
 }
 
 export async function completeDevice(
-  handle: string,
-  device: Device
+    handle: string,
+    device: Device
 ): Promise<void> {
-  // check if prints match
-  try {
-    const user = await getUser(handle);
-    if (device.id in user.devices) {
-      // check the prints
-      if (device.fingerprint !== user.devices[device.id].fingerprint) {
+    // check if prints match
+    try {
+        const user = await getUser(handle);
+        if (device.id in user.devices) {
+            // check the prints
+            if (device.fingerprint !== user.devices[device.id].fingerprint) {
+                throw new Error('Authentication Error');
+            } else {
+                // ready!
+                user.devices[device.id] = device;
+                await updateUser(user);
+            }
+        } else {
+            throw new Error('Authentication Error');
+        }
+    } catch (e) {
         throw new Error('Authentication Error');
-      } else {
-        // ready!
-        user.devices[device.id] = device;
-        await updateUser(user);
-      }
-    } else {
-      throw new Error('Authentication Error');
     }
-  } catch (e) {
-    throw new Error('Authentication Error');
-  }
 }
